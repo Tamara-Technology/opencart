658d55b79ad93a58a1b19e4052bf0a0ac366c265
Chien Hoang <chien.hoang@tamara.co>
2021-03-04 11:58:42 +0100

    Attach vendor

---
+++ vendor/cardinity/cardinity-sdk-php/spec/Method/Void/VoidSpec.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace spec\Cardinity\Method\Void;
+
+use PhpSpec\ObjectBehavior;
+
+class VoidSpec extends ObjectBehavior
+{
+    function it_implements_result_object_behaviour()
+    {
+        $this->shouldImplement('Cardinity\Method\ResultObjectInterface');
+    }
+
+    function it_is_serializable()
+    {
+        $this->shouldImplement('\Serializable');
+    }
+}
