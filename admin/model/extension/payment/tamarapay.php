8c002741af652464ef6f18c89665aafc379e5a38
Chien Hoang <chien.hoang@tamara.co>
2021-06-13 10:57:53 +0200

    update version

--- admin/model/extension/payment/tamarapay.php
+++ admin/model/extension/payment/tamarapay.php
@@ -9,7 +9,7 @@ class ModelExtensionPaymentTamarapay extends Model
     /**
      * Define version of extension
      */
-    public const VERSION = '1.6.1';
+    public const VERSION = '1.6.2';
 
     /**
      * Define schema version
