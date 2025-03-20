4f7db6c36dd9de71db984f51cdc3b64c66877906
Cuong Huynh <cuong.huynh@tamara.co>
2025-03-20 11:26:06 +0400

    1.5.0

--- admin/model/payment/tamarapay.php
+++ admin/model/payment/tamarapay.php
@@ -9,7 +9,7 @@ class ModelPaymentTamarapay extends Model
     /**
      * Define version of extension
      */
-    public const VERSION = '1.4.9';
+    public const VERSION = '1.5.0';
 
     /**
      * Define schema version
--- catalog/model/payment/tamarapay.php
+++ catalog/model/payment/tamarapay.php
@@ -24,7 +24,7 @@ class ModelPaymentTamarapay extends Model
     /**
      * Define version of extension
      */
-    public const VERSION = '1.4.9';
+    public const VERSION = '1.5.0';
 
     public const
         MAX_LIMIT = 'max_limit',
